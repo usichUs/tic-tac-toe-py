1f368edbed16289a03edb51319719ec5c7836c32
Nikita Usachev <guyfoks737@gmail.com>
2024-10-27 21:32:08 +0300

    console part done

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -17,6 +17,35 @@ class Board:
             print('|'.join(row))
             print('-' * 5)
 
+    def is_board_full(self):
+        # Цикл проходится по всем столбцам игрового поля.
+        for i in range(self.field_size):
+            # А потом по всем строчкам.
+            for j in range(self.field_size):
+                # Если находит свободную ячейку...
+                if self.board[i][j] == ' ':
+                    # ...игра продолжается.
+                    return False
+        # Иначе - ничья!
+        return True
+    
+
+    def check_win(self, player):
+        # Тут реализована проверка по вертикали и горизонтали.
+        for i in range(3):
+            if (all([self.board[i][j] == player for j in range(3)]) or
+                    all([self.board[j][i] == player for j in range(3)])):
+                return True
+        # Тут реализована проверка по диагонали.
+        if (
+            self.board[0][0] == self.board[1][1] == self.board[2][2] == player
+            or
+            self.board[0][2] == self.board[1][1] == self.board[2][0] == player
+        ):
+            return True
+
+        return False
+
     def __str__(self):
         return (
             'Объект игрового поля размером '
